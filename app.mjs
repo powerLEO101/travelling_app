ccd0e041e9df3b3b9ac984ba8ec55b6b2bf34880
Abid2422 <akhoundabid22@gmail.com>
2025-03-01 11:18:08 -0500

    Changed some stuff

--- app.mjs
+++ app.mjs
@@ -7,3 +7,21 @@ import fs from 'fs';
 
 export let server = null;
 export const app = express();
+
+server = app.listen(3000);
+console.log('Server started; type CTRL+C to shut down');
+
+app.set('view engine', 'hbs');
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    console.log(req.query);
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.render('mainPage');
+});
